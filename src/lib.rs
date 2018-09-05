7ba5c2a0991c5027af180555e1c3c55a99a0930d
Chris <christopherwong888@gmail.com>
2018-09-04 19:08:31 -0700

    Added license/citations

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/**
+ * If you use the VADER sentiment analysis tools, please cite:
+ * Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
+ * Sentiment Analysis of Social Media Text. Eighth International Conference on
+ * Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+ **/
+
+
 #[macro_use] extern crate maplit;
 #[macro_use] extern crate lazy_static;
 extern crate regex;
